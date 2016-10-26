979f239becb702264fd2d28e8e3a44b34fa41adf
Kostya Belyi <Kbelyi@list.ru>
2016-10-26 19:59:42 +0300

    Lesson_26_10_16

---
+++ Test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Константин
+ * Date: 23.10.2016
+ * Time: 22:54
+ */
+
+    $my_arr = array("Ukraine" => "Kiev",
+        "Russia" => "Moscow",
+        "USA" => "Washington",
+        "Italy" => "Rome",
+        "France" => "Paris");
+
+    echo "<pre>";
+    print_r($my_arr);
+    echo "</pre>";
