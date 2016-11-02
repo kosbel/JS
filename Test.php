3dfce24788bd186a96a1b02f95997bc186be1bfb
Kostya Belyi <Kbelyi@list.ru>
2016-11-02 19:13:06 +0200

    Lesson_28.10.16

--- Test.php
+++ Test.php
@@ -1,17 +1,42 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Константин
- * Date: 23.10.2016
- * Time: 22:54
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="get">
+        <label>
+            <input type="text" name="text" value="asd">
+        </label>
+        <label>
+            <input type="email" name="email" value="asdsd@asds">
+        </label>
+        <input type="submit">
+    </form>
 
-    $my_arr = array("Ukraine" => "Kiev",
-        "Russia" => "Moscow",
-        "USA" => "Washington",
-        "Italy" => "Rome",
-        "France" => "Paris");
+    <?php
+
+    print_r($_GET);
+    echo "</br>";
 
     echo "<pre>";
-    print_r($my_arr);
+    print_r($_SERVER);
     echo "</pre>";
+//        $my_arr = array("Ukraine" => "Kiev",
+//            "Russia" => "Moscow",
+//            "USA" => "Washington",
+//            "Italy" => "Rome",
+//            "France" => "Paris");
+//
+//            echo "<pre>";
+//            print_r($my_arr);
+//            echo "</pre>";
+    ?>
+</body>
+</html>
+
+
